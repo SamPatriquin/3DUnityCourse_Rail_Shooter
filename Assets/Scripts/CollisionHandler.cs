34a953379fd260bcbc9269c9a02c7ae6313ff8b2
SamPatriquin <49535194+SamPatriquin@users.noreply.github.com>
2020-05-12 23:23:46 -0400

    Explosion on death

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -2,9 +2,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; //Only script that is loading scenes so it's ok
 
 public class CollisionHandler : MonoBehaviour
 {
+
+    [SerializeField] float levelResetDelay = 2f;
+    [SerializeField] GameObject deathEffect;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,5 +27,11 @@ public class CollisionHandler : MonoBehaviour
     }
     private void proccessDeath() {
         SendMessage("onPlayerDeath"); //Calls function in other scripts within the game object
+        deathEffect.SetActive(true);
+        Invoke("resetLevel", levelResetDelay);
+    }
+
+    private void resetLevel() {
+        SceneManager.LoadScene(1);
     }
 }
